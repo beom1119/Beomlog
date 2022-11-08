de368127f91dc602375375728186052eda7dd344
beombeom1119 <qjarl1119@naver.com>
2022-11-08 21:06:10 +0900

    ""

--- src/main/java/com/Beomlog/controller/PostController.java
+++ src/main/java/com/Beomlog/controller/PostController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController     /// 그냥 컨트롤러도 가능한데 데이터 기반으로 사용하기 때문에 restController 사용
 public class PostController {
 
-    @GetMapping("/hello")
+    @GetMapping("/posts")
     public String get()
     {
         return "hello world";
---
+++ src/test/java/com/Beomlog/controller/PostControllerTest.java
@@ -0,0 +1,37 @@
+package com.Beomlog.controller;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@WebMvcTest
+class PostControllerTest {
+
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Test
+    @DisplayName("/Post 요청 시 Hello World를 출력한다.")
+    void test() throws Exception {
+
+        //expected
+        mockMvc.perform(MockMvcRequestBuilders.get("/posts"))
+                .andExpect(status().isOk())
+                .andExpect(content().string("hello world"))
+                .andDo(print());
+    }
+
+
+
+
+
+}
